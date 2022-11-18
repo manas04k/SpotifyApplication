a6f36c52131a4911da8bf372ad05d4de42c0b64c
Asus <khitoliyamanas@gmail.com>
2022-11-18 16:49:31 +0530

    Complete Application

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         Album album1 = new Album("Album1", "Alan Walker");
-        album1.addSong("Alone", 5.5);
-        album1.addSong("cee", 56.3);
-        album1.addSong("Faded", 7.65);
-        album1.addSong("rtt", 67);
+        album1.addSong("Song 1", 5.5);
+        album1.addSong("Song 2", 56.3);
+        album1.addSong("Song 3", 7.65);
+        album1.addSong("Song 4", 67);
+        album1.addSong("Song 5", 6.7);
+        album1.addSong("Song 6", 6.90);
+        album1.addSong("Song 7", 4.7);
+        album1.addSong("Song 8", 8.7);
 
         LinkedList<Song> myplaylist = new LinkedList<>();
-        album1.addtoPlaylist("Alone", myplaylist);
-        album1.addtoPlaylist("Faded", myplaylist);
-        album1.addtoPlaylist("Alonee", myplaylist);
+        album1.addtoPlaylist("Song 1", myplaylist);
+        album1.addtoPlaylist("Song 2", myplaylist);
+        album1.addtoPlaylist("Song 3", myplaylist);
+        album1.addtoPlaylist("Song 4", myplaylist);
+        album1.addtoPlaylist("Song 5", myplaylist);
+        album1.addtoPlaylist("Song 6", myplaylist);
 
         printplaylist(myplaylist);
         play(myplaylist);
@@ -32,6 +39,7 @@ public class Main {
         System.out.println("You are now listening " + itr.next());
         showMenu();
 
+        boolean forward = true;
         while(true){
             int option = sc.nextInt();
 
@@ -48,7 +56,74 @@ public class Main {
                     printplaylist(playlist);
                     break;
 
+                case 3:
+                    if(!forward){
+                        if(itr.hasNext()){
+                            itr.next();
+                        }
+//                        else{
+//                              System.out.println("No Next Song");
+//                        }
+                    }
+                    if(!itr.hasNext()){
+                        System.out.println("You have Reached End of the Playlist");
+                    }
+                    else{
+                        System.out.println("You are listening to " + itr.next());
+                    }
+                    forward = true;
+                    break;
 
+                case 4:
+                    if(forward){
+                        if(itr.hasPrevious()){
+                            itr.previous();
+                        }
+//                        else{
+//                            System.out.println("No Previous Song");
+//                        }
+                    }
+                    if(!itr.hasPrevious()){
+                        System.out.println("You have Reached Start of the Playlist");
+                    }
+                    else{
+                        System.out.println("You are listening to " + itr.previous());
+                    }
+                    forward = false;
+                    break;
+
+                case 5:
+//
+                    if(forward){
+                        System.out.println("You are listening to " + itr.previous());
+                        forward = false;
+                        break;
+                    }
+                    else{
+                        System.out.println("You are listening to " + itr.next());
+                        forward = true;
+                        break;
+                    }
+
+                case 6:
+                    if(playlist.size()>0){
+
+                        itr.remove();
+                        System.out.println("Song removed Succesfully");
+                        if(itr.hasNext()){
+                            System.out.println("You are listening to " + itr.next());
+                        }
+                        else if(itr.hasPrevious()){
+                            System.out.println("You are listening to " + itr.previous());
+                        }
+                        else{
+                            System.out.println("Empty Playlist");
+                        }
+                        break;
+
+                    }
+                    System.out.println("Empty Playlist");
+                    break;
 
 
             }
